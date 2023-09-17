76df72f9e5a19ea609de8084c25c11d28772a128
zaidgatta <135795549+zaidgatta@users.noreply.github.com>
2023-09-17 04:16:08 -0700

    Add files via upload

---
+++ admin_header.php
@@ -0,0 +1,26 @@
+<header class="header">
+
+   <div id="close-btn"><i class="fas fa-times"></i></div>
+
+   <a href="dashboard.php" class="logo">AdminPanel.</a>
+
+   <nav class="navbar">
+      <a href="dashboard.php"><i class="fas fa-home"></i><span>home</span></a>
+      <a href="listings.php"><i class="fas fa-building"></i><span>listings</span></a>
+      <a href="users.php"><i class="fas fa-user"></i><span>users</span></a>
+      <a href="admins.php"><i class="fas fa-user-gear"></i><span>admins</span></a>
+      <a href="messages.php"><i class="fas fa-message"></i><span>messages</span></a>
+      <a href="post_property.php"><span><i class="fa-solid fa-building"></i>property</span></a>
+      <a href="requests.php"><span><i class="fa-solid fa-rectangle-list"></i>requests</span></a>
+   </nav>
+
+   <a href="update.php" class="btn">update account</a>
+   <div class="flex-btn">
+      <a href="index.php" class="option-btn">login</a>
+      <a href="register.php" class="option-btn">register</a>
+   </div>
+   <a href="admin_logout.php" onclick="return confirm('logout from this website?');" class="delete-btn"><i class="fas fa-right-from-bracket"></i><span>logout</span></a>
+
+</header>
+
+<div id="menu-btn" class="fas fa-bars"></div>
---
+++ dashboard.php
@@ -0,0 +1,151 @@
+<?php
+
+include 'connect.php';
+
+if(isset($_COOKIE['admin_id'])){
+   $admin_id = $_COOKIE['admin_id'];
+}else{
+   $admin_id = '';
+   header('location:login.php');
+}
+if(isset($_COOKIE['user_id'])){
+   $user_id = $_COOKIE['user_id'];
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Dashboard</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="admin_style.css">
+
+</head>
+<body>
+   
+<!-- header section starts  -->
+<?php include 'admin_header.php'; ?>
+<!-- header section ends -->
+
+<!-- dashboard section starts  -->
+
+<section class="dashboard">
+
+   <h1 class="heading">dashboard</h1>
+
+   <div class="box-container">
+
+   <div class="box">
+      <?php
+         $select_profile = $conn->prepare("SELECT * FROM `admins` WHERE id = ? LIMIT 1");
+         $select_profile->execute([$admin_id]);
+         $fetch_profile = $select_profile->fetch(PDO::FETCH_ASSOC);
+      ?>
+      <h3>welcome!</h3>
+      <p><?= $fetch_profile['name']; ?></p>
+      <a href="update.php" class="btn">update profile</a>
+   </div>
+
+   <div class="box">
+      <?php
+         $select_listings = $conn->prepare("SELECT * FROM `property`");
+         $select_listings->execute();
+         $count_listings = $select_listings->rowCount();
+      ?>
+      <h3><?= $count_listings; ?></h3>
+      <p>property posted</p>
+      <a href="listings.php" class="btn">view listings</a>
+   </div>
+
+   <div class="box">
+      <?php
+         $select_users = $conn->prepare("SELECT * FROM `users`");
+         $select_users->execute();
+         $count_users = $select_users->rowCount();
+      ?>
+      <h3><?= $count_users; ?></h3>
+      <p>total users</p>
+      <a href="users.php" class="btn">view users</a>
+   </div>
+
+   <div class="box">
+      <?php
+         $select_admins = $conn->prepare("SELECT * FROM `admins`");
+         $select_admins->execute();
+         $count_admins = $select_admins->rowCount();
+      ?>
+      <h3><?= $count_admins; ?></h3>
+      <p>total admins</p>
+      <a href="admins.php" class="btn">view admins</a>
+   </div>
+
+   <div class="box">
+      <?php
+         $select_messages = $conn->prepare("SELECT * FROM `messages`");
+         $select_messages->execute();
+         $count_messages = $select_messages->rowCount();
+      ?>
+      <h3><?= $count_messages; ?></h3>
+      <p>new messages</p>
+      <a href="messages.php" class="btn">view messages</a>
+   </div>
+
+   <div class="box">
+      <p>post property</p>
+      <a href="post_property.php" class="btn">post property</a>
+   </div>
+
+   <div class="box">
+      <?php
+        $count_requests_received = $conn->prepare("SELECT * FROM `requests` WHERE receiver = ?");
+        $count_requests_received->execute([$user_id]);
+        $total_requests_received = $count_requests_received->rowCount();
+      ?>
+      <h3><?= $total_requests_received; ?></h3>
+      <p>requests received</p>
+      <a href="requests.php" class="btn">view all requests</a>
+      </div>
+
+   </div>
+
+</section>
+
+
+<!-- dashboard section ends -->
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/2.1.2/sweetalert.min.js"></script>
+
+<!-- custom js file link  -->
+<script src="admin_script.js"></script>
+
+<?php include 'message.php'; ?>
+
+</body>
+</html>
